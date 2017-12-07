1e3cf585067693b203e6450959fd9e23cd5b1ddc
r.uchiyama <r.uchiyama@sint.co.jp>
2017-12-07 11:31:47 +0900

    Hash値計算をUtility化

---
+++ src/BlockChain/Util/Hash.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain.Util
+{
+    public class Hash
+    {
+        /// <summary>
+        /// ハッシュを計算する
+        /// </summary>
+        /// <typeparam name="T">HashAlgorihm</typeparam>
+        /// <param name="target">ハッシュ変換元</param>
+        /// <param name="hashAlgorithm">ハッシュアルゴリズム Nullの場合、Tの既定ハッシュアルゴリズムを使用</param>
+        /// <returns></returns>
+        public static byte[] GetHash<T>(byte[] target,T hashAlgorithm) where T : HashAlgorithm, new()
+        {
+            if (hashAlgorithm == null)
+            {
+                using (var hashProvider = new T())
+                {
+                    return hashProvider.ComputeHash(target);
+                }
+            }
+            else
+            {
+                return hashAlgorithm.ComputeHash(target);
+            }
+        }
+
+        /// <summary>
+        /// ファイルのハッシュを計算する
+        /// </summary>
+        /// <typeparam name="T">HashAlgorithm</typeparam>
+        /// <param name="fileName">ファイル名</param>
+        /// <param name="hashAlgorithm">ハッシュアルゴリズム Nullの場合、Tの既定ハッシュアルゴリズムを使用</param>
+        /// <returns></returns>
+        public static byte[] GetHashFromFile<T>(string fileName, T hashAlgorithm) where T : HashAlgorithm, new()
+        {
+            try
+            {
+                using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    if (hashAlgorithm == null)
+                    {
+                        using (var hashProvider = new T())
+                        {
+                            return hashProvider.ComputeHash(fileStream);
+                        }
+                    }
+                    else
+                    {
+                        return hashAlgorithm.ComputeHash(fileStream);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                // FileStream周りでExceptionが発生する可能性あり
+                // 例外処理をそのままスローする
+                throw;
+            }
+        }
+
+        public static string ConvertHashString(byte[] hash)
+        {
+            if (hash == null)
+            {
+                throw new ArgumentNullException("hash");
+            }
+
+            return BitConverter.ToString(hash).ToLower(CultureInfo.CurrentCulture).Replace("-", "");
+        }
+    }
+}
