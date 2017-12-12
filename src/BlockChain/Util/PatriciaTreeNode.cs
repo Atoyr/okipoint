b440bcc660b9b656def42eba2cb795b6da9e0948
r.uchiyama <r.uchiyama@sint.co.jp>
2017-12-12 11:37:00 +0900

    PatriciaTree追加

---
+++ src/BlockChain/Util/PatriciaTreeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain.Util
+{
+    class PatriciaTreeNode<T> : PrefixTreeNode<T>
+    {
+        public List<int> ShortcutPass { set; get; } = new List<int>();
+
+        PatriciaTreeNode()
+        {
+            Children = new PatriciaTreeNode<T>[ARRAY_LENGTH];
+        }
+    }
+}
