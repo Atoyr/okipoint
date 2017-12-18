1304b884f88f809dc18a707032be5aac3deb48f9
r.uchiyama <r.uchiyama@sint.co.jp>
2017-12-18 19:32:33 +0900

    CoinValueの追加

---
+++ src/Blockchain.Api/Models/CoinValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockchain.Api.Models
+{
+    public class CoinValue : IEquatable<CoinValue>
+    {
+        public bool Equals(CoinValue other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
