63c50d50c31ecd0ced44f4017dcfb3f7f0ef01e5
r.uchiyama <r.uchiyama@sint.co.jp>
2017-12-18 19:32:19 +0900

    Outputの追加

---
+++ src/Blockchain.Api/Models/Output.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Blockchain.Api.Models
+{
+    public class Output
+    {
+        private Output()
+        {
+
+        }
+
+        public string Address { get; private set; }
+
+        public long TxIndex { get; private set; }
+
+        public string Script { get; private set; }
+
+        public bool Spent { get; private set; }
+    }
+}
