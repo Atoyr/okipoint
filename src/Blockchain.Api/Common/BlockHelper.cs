26c68cfe9f25204f8cc90da66accd6521bbbeaf6
Atoyr <r.uchiyama@sint.co.jp>
2017-12-20 13:21:10 +0900

    BlockHelper作成

---
+++ src/Blockchain.Api/Common/BlockHelper.cs
@@ -0,0 +1,25 @@
+﻿using Blockchain.Api.Models;
+using Blockchain.Api.Utility;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blockchain.Api.Common
+{
+    public static class BlockHelper
+    {
+        public static string GetHash(Block block)
+        {
+            var bytes = new List<byte>();
+            bytes.AddRange(Encoding.ASCII.GetBytes(block.PreviousHash));
+            bytes.AddRange(BitConverter.GetBytes(block.Timestamp.ToUniversalTime().ToBinary()));
+            foreach (Transaction t in block.Transactions)
+            {
+                bytes.AddRange(t.ToBytes());
+            }
+            bytes.AddRange(BitConverter.GetBytes(block.Nonce));
+            return Encoding.ASCII.GetString(Hash.GetHash<SHA256Managed>(bytes.ToArray()));
+        }
+    }
+}
