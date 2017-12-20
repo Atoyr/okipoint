23db2edc95d1be67840c3c3e126d63f1c6a8477a
Atoyr <r.uchiyama@sint.co.jp>
2017-12-20 12:25:56 +0900

    TransactionHelperクラス作成

---
+++ src/Blockchain.Api/Common/TransactionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blockchain.Api.Models;
+
+namespace Blockchain.Api.Common
+{
+    public static class TransactionHelper
+    {
+        public static Transaction CreateTransaction(ICollection<Input> inputs, ICollection<Output> outputs) => new Transaction(inputs, outputs);
+    }
+}
