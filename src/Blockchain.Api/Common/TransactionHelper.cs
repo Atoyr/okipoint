f5fa7c24c674a26e64ea5b9df57885a5d453a188
Atoyr <lessironglance@gmail.com>
2017-12-21 00:20:16 +0900

    トランザクション生成時にInput、Outputの値検証をするよう修正

--- src/Blockchain.Api/Common/TransactionHelper.cs
+++ src/Blockchain.Api/Common/TransactionHelper.cs
@@ -7,6 +7,22 @@ namespace Blockchain.Api.Common
 {
     public static class TransactionHelper
     {
-        public static Transaction CreateTransaction(ICollection<Input> inputs, ICollection<Output> outputs) => new Transaction(inputs, outputs);
+        public static Transaction CreateTransaction(ICollection<Input> inputs, ICollection<Output> outputs)
+        {
+            decimal Amount = 0;
+
+            foreach(Input i in inputs)
+            {
+                if (i.PreviousOutput.Value < 0) throw new ArgumentException(nameof(inputs));
+                Amount += i.PreviousOutput.Value;
+            }
+            foreach (Output o in outputs)
+            {
+                if (o.Value < 0) throw new ArgumentException(nameof(outputs));
+                Amount -= o.Value;
+            }
+            if (Amount < 0) throw new ArgumentException(nameof(inputs));
+            return new Transaction(inputs, outputs);
+        }
     }
 }
